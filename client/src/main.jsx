4ca9c3e46b88f91c20812eb01582021e0d83f96f
Lindsay Terry <lindsaytee66@gmail.com>
2025-01-02 11:59:25 -0700

    Sets up routing for client side and creates homepage element

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
+import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom';
+import router from './Routing.jsx';
+import 'semantic-ui-css/semantic.min.css';
+import './index.css';
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
