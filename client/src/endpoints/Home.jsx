49dc151e8b7670fef066de9719be4ba2e5ff3230
Lindsay Terry <lindsaytee66@gmail.com>
2025-01-03 17:50:37 -0700

    Creates search bar to search movies/tv shows

--- client/src/endpoints/Home.jsx
+++ client/src/endpoints/Home.jsx
@@ -1,7 +1,43 @@
+import { useState } from 'react';
+import { FormField, Form, FormInput, Button, ButtonGroup, ButtonOr } from 'semantic-ui-react';
+
 export default function Home() {
+    const [formData, setFormData] = useState({
+        search: '',
+    });
+
+    const handleSearch = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Form submitted', formData.search);
+    };
+
+    const handleCancel = () => {
+        setFormData({
+            search: '',
+        });
+    };
+
+
     return (
         <div>
-            <p>HOMEPAGE</p>
+            {/* <p>HOMEPAGE</p> */}
+            <Form onSubmit={handleSubmit}>
+                <FormField>
+                    <FormInput name="search" id="search" value={formData.search} onChange={handleSearch} placeholder="Search for a TV show or movie"/>
+                </FormField>
+                <ButtonGroup>
+                    <Button type="button" onClick={handleCancel}>Cancel</Button>
+                    <ButtonOr />
+                    <Button type="submit">Search</Button>
+                </ButtonGroup>
+            </Form>
         </div>
     )
 }
