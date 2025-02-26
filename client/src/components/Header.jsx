2d6075073a94fda2a0122e590e87f9c8ae79724b
Lindsay Terry <lindsaytee66@gmail.com>
2025-02-26 16:47:31 -0700

    Adds login button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,18 @@
 import SignUp from '../components/SignUp';
+import Login from '../components/Login';
+// import Button from 'react-bootstrap/Button';
 
 export default function Header() {
 
     return (
         <div className='d-flex justify-content-between'>
             <p>HEADER</p>
-            <div>
+            <div className='d-flex'>
+                {/* <Button>Login</Button>
+                <Button>Get Started</Button> */}
+                <Login />
                 <SignUp />
             </div>
-            
         </div>
     )
 }
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button';
+
+export default function Login() {
+
+    return (
+        <div className='d-flex'>
+            <Button className="m-4">Login</Button> 
+        </div>
+    )
+}
